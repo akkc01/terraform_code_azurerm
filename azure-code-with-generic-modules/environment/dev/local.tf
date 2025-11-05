80c7a0a44b2795c902f3b39a6bfa5c6762fa2578
A K Verma <amitkkc01@gmail.com>
2025-11-06 00:11:02 +0530

    Delete azure-code-with-generic-modules/environment/dev directory

--- azure-code-with-generic-modules/environment/dev/local.tf
+++
@@ -1,9 +0,0 @@
-# # Auto generate mapping for association module
-# locals {
-#   nic_nsg_map = {
-#     for key in keys(module.network_interface.nic_ids) : key => {
-#       network_interface_id      = module.network_interface.nic_ids[key]
-#       network_security_group_id = module.nsg.nsg_ids[key]
-#     }
-#   }
-# }
