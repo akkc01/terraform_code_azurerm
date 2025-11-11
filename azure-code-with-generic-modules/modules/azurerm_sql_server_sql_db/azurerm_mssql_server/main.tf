55dcaae2b71b52e6ceeb72ee6a4df0846d90b0ae
akkc01 <amitkkc01@gmail.com>
2025-11-11 21:34:54 +0530

    sl

--- azure-code-with-generic-modules/modules/azurerm_sql_server_sql_db/azurerm_mssql_server/main.tf
+++ azure-code-with-generic-modules/modules/azurerm_sql_server_sql_db/azurerm_mssql_server/main.tf
@@ -1,15 +1,10 @@
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-resource "azurerm_user_assigned_identity" "example" {
-  name                = "example-admin"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
+# resource "azurerm_user_assigned_identity" "example" {
+#   name                = "example-admin"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+# }
 
 resource "azurerm_mssql_server" "example" {
   name                         = "example-resource"
@@ -20,54 +15,16 @@ resource "azurerm_mssql_server" "example" {
   administrator_login_password = "Example_Password!"
   minimum_tls_version          = "1.2"
 
-  azuread_administrator {
-    login_username = azurerm_user_assigned_identity.example.name
-    object_id      = azurerm_user_assigned_identity.example.principal_id
-  }
+  # azuread_administrator {
+  #   login_username = azurerm_user_assigned_identity.example.name
+  #   object_id      = azurerm_user_assigned_identity.example.principal_id
+  # }
 
-  identity {
-    type         = "UserAssigned"
-    identity_ids = [azurerm_user_assigned_identity.example.id]
-  }
+  # identity {
+  #   type         = "UserAssigned"
+  #   identity_ids = [azurerm_user_assigned_identity.example.id]
+  # }
 
-  primary_user_assigned_identity_id            = azurerm_user_assigned_identity.example.id
-  transparent_data_encryption_key_vault_key_id = azurerm_key_vault_key.example.id
-}
-
-# Create a key vault with access policies which allow for the current user to get, list, create, delete, update, recover, purge and getRotationPolicy for the key vault key and also add a key vault access policy for the Microsoft Sql Server instance User Managed Identity to get, wrap, and unwrap key(s)
-resource "azurerm_key_vault" "example" {
-  name                        = "mssqltdeexample"
-  location                    = azurerm_resource_group.example.location
-  resource_group_name         = azurerm_resource_group.example.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = azurerm_user_assigned_identity.example.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = true
-
-  sku_name = "standard"
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = ["Get", "List", "Create", "Delete", "Update", "Recover", "Purge", "GetRotationPolicy"]
-  }
-
-  access_policy {
-    tenant_id = azurerm_user_assigned_identity.example.tenant_id
-    object_id = azurerm_user_assigned_identity.example.principal_id
-
-    key_permissions = ["Get", "WrapKey", "UnwrapKey"]
-  }
-}
-
-resource "azurerm_key_vault_key" "example" {
-  depends_on = [azurerm_key_vault.example]
-
-  name         = "example-key"
-  key_vault_id = azurerm_key_vault.example.id
-  key_type     = "RSA"
-  key_size     = 2048
-
-  key_opts = ["unwrapKey", "wrapKey"]
+  # primary_user_assigned_identity_id            = azurerm_user_assigned_identity.example.id
+  # transparent_data_encryption_key_vault_key_id = azurerm_key_vault_key.example.id
 }
